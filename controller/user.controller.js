b294c852d78a7cc2b183a8eca3f0f413ce56ad28
neon2k21 <roman_2001@icloud.com>
2024-01-28 11:39:23 +0500

    edited

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,25 +4,38 @@ class UserController{
     async createUser(req,res){
         
         const {
-            id,
-            name,
-            surname,
+            
+            fio,
             login,
             password,
             role,
             phone
         } = req.body
-        console.log( name, surname, login, password, role, phone)
+        console.log( fio, login, password, role, phone)
+
         const newUser = await db.query(
-            `insert into hackaton."Users" (id, "Name", "Surname", login, password, role, phone) values ($1, $2, $3, $4, $5, $6, $7) returning *`,
-             [ id, name, surname, login, password, role, phone]
+            `insert into users (fio, login, password, role, phone) values ($1, $2, $3, $4, $5) returning *`,
+             [ fio, login, password, role, phone]
         )
         res.json(newUser.rows[0])
     }   
     async getAllUsers(req,res){
+        const {
+            login,
+            password} = req.body 
+
+        const allUsers = await db.query(
+            `select * from Users where login=$1 AND password=$2`, [login,password]
+        )
+        
+
+        if(allUsers.rowCount===0) res.json('Логин или пароль не тот лол, а возможно и не зареган кек')
+        else res.json(allUsers.rows[0].id)
 
     }
     async getOneUser(req,res){
+
+
 
     }
     async updateUser(req,res){
